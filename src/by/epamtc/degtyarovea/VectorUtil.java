5a4225a7c73355f4c01e8e1abb0d869579abbeed
degtyaryov <Degtyarov96@mail.ru>
2020-06-21 12:48:08 +0300

    Refactoring of one dimension array class

--- src/by/epamtc/degtyarovea/VectorUtil.java
+++ src/by/epamtc/degtyarovea/VectorUtil.java
@@ -4,25 +4,25 @@ import java.util.Arrays;
 import java.util.Map;
 import java.util.TreeMap;
 
-public class OneDimensionArrayHelper {
+public class VectorUtil {
 
     public static void main(String[] args) {
-
-        double[] doubleNums = {-14, -3, 2, -1, 0, 1, 2, 3};
+        double[] doubleNums = {-14.1, -3.2, 2, -1, 0, 1, 2.4, 3};
         int[] integerNums = {1, 2, 3, 0, 1, 3, 3};
-        int[] initPassword = {0, 0, 0, 0, 0, 0, 0, 0, 1, 5};
 
         // Task 1
-        System.out.printf("Max sum near nums: %.2f.%n",
-                maxSumNextNums(doubleNums));
+        System.out.printf("Max sum of nums nearby: %.2f.%n",
+                maxSumNearbyNums(doubleNums));
 
         // Task 2
         System.out.printf("Array without min number: %s.%n",
-                Arrays.toString(deleteMinNum(integerNums)));
+                Arrays.toString(deleteMinNums(integerNums)));
 
         // Task 3
-        System.out.printf("Password: %s.%n",
-                Arrays.toString(getPassword(initPassword)));
+        int[] initPassword = {0, 0, 0, 0, 0, 0, 0, 0, 1, 5};
+        int[] fullPassword = getPassword(initPassword);
+        String passwordText = (fullPassword != null) ? Arrays.toString(fullPassword) : "Password not found";
+        System.out.printf("Password: %s.%n", passwordText);
 
         // Task 4
         System.out.printf("Most common number is: %.2f.%n",
@@ -33,8 +33,8 @@ public class OneDimensionArrayHelper {
                 getSumMaxEvenAndMinOdd(doubleNums));
     }
 
-    public static double maxSumNextNums(double[] nums) {
-        if (!hasElements(nums)) {
+    public static double maxSumNearbyNums(double[] nums) {
+        if (isEmpty(nums)) {
             return 0;
         } else if (nums.length == 1) {
             return nums[0];
@@ -53,11 +53,11 @@ public class OneDimensionArrayHelper {
         }
     }
 
-    private static boolean hasElements(double[] nums) {
-        return nums != null && nums.length > 0;
+    private static boolean isEmpty(double[] nums) {
+        return nums == null || nums.length == 0;
     }
 
-    public static int[] deleteMinNum(int[] nums) {
+    public static int[] deleteMinNums(int[] nums) {
         if (nums == null || nums.length == 0) {
             return nums;
         } else {
@@ -99,6 +99,43 @@ public class OneDimensionArrayHelper {
         return count;
     }
 
+    public static int[] getPassword(int[] array) {
+        int[] password = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        int indexFirstCube = 0;
+        int indexSecondCube = 0;
+
+        // find initial cubes
+        for (int i = 0; i < array.length; i++) {
+            indexFirstCube = array[indexFirstCube] > 0 ? indexFirstCube : i;
+            indexSecondCube = array[indexFirstCube] > 0 && array[i] > 0 ? i : indexSecondCube;
+        }
+
+        int[] sequence = findSequenceOfThree(array, indexFirstCube, indexSecondCube);
+
+        for (int i = 0; i < password.length; i++) {
+            password[i] = sequence[i % 3];
+        }
+
+        if (password[indexFirstCube] != array[indexFirstCube]
+                || password[indexSecondCube] != array[indexSecondCube]) {
+            return null;
+        } else {
+            return password;
+        }
+    }
+
+    private static int[] findSequenceOfThree(int[] array, int indexFirstCube, int indexSecondCube) {
+        int[] sequence = new int[3];
+        int pos2 = (indexSecondCube - indexFirstCube) % 3 > 1 ? 2 : 1;
+        int pos3 = (indexSecondCube - indexFirstCube) % 3 > 1 ? 1 : 2;
+
+        sequence[indexFirstCube % 3] = array[indexFirstCube];
+        sequence[(indexFirstCube + pos2) % 3] = array[indexSecondCube];
+        sequence[(indexFirstCube + pos3) % 3] = 10 - array[indexFirstCube] - sequence[(indexFirstCube + pos2) % 3];
+
+        return sequence;
+    }
+
     public static double getMostCommonNum(double[] nums) {
         Map<Double, Integer> numsFrequency = getNumsFrequency(nums);
         double mostCommonNum = 0;
@@ -108,7 +145,8 @@ public class OneDimensionArrayHelper {
             Double num = numWithFrequency.getKey();
             Integer frequency = numWithFrequency.getValue();
 
-            if (frequency > maxFrequency || (frequency == maxFrequency && numWithFrequency.getKey() < mostCommonNum)) {
+            if (frequency > maxFrequency
+                    || (frequency == maxFrequency && numWithFrequency.getKey() < mostCommonNum)) {
                 mostCommonNum = num;
                 maxFrequency = frequency;
             }
@@ -120,12 +158,11 @@ public class OneDimensionArrayHelper {
     private static Map<Double, Integer> getNumsFrequency(double[] nums) {
         Map<Double, Integer> numFrequency = new TreeMap<>();
 
-        for (int i = 0; i < nums.length; i++) {
-            double num = nums[i];
+        for (double num : nums) {
             Integer frequency = numFrequency.get(num);
 
             if (frequency != null) {
-                numFrequency.put(num, frequency + 1);
+                numFrequency.put(num, ++frequency);
             } else {
                 numFrequency.put(num, 1);
             }
@@ -135,7 +172,7 @@ public class OneDimensionArrayHelper {
     }
 
     public static double getSumMaxEvenAndMinOdd(double[] nums) {
-        if (!hasElements(nums)) {
+        if (isEmpty(nums)) {
             return 0;
         }
         double maxEven = getMaxEven(nums);
@@ -166,40 +203,5 @@ public class OneDimensionArrayHelper {
         }
 
         return minOdd;
-    }
-
-    public static int[] getPassword(int[] array) {
-        int[] password = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-        int indexFirstCube = 0;
-        int indexSecondCube = 0;
-
-        // find initial cubes
-        for (int i = 0; i < array.length; i++) {
-            indexFirstCube = array[indexFirstCube] > 0 ? indexFirstCube : i;
-            indexSecondCube = array[indexFirstCube] > 0 && array[i] > 0 ? i : indexSecondCube;
-        }
-
-        // find sequence of 3 cubes
-        int pos2 = (indexSecondCube - indexFirstCube) % 3 > 1 ? 2 : 1;
-        int pos3 = (indexSecondCube - indexFirstCube) % 3 > 1 ? 1 : 2;
-        int[] d = new int[3];
-        d[indexFirstCube % 3] = array[indexFirstCube];
-        d[(indexFirstCube + pos2) % 3] = array[indexSecondCube];
-        d[(indexFirstCube + pos3) % 3] = 10 - array[indexFirstCube] - d[(indexFirstCube + pos2) % 3];
-
-
-        // fill password and check is valid
-        for (int i = 0; i < password.length; i++) {
-            if (d[i % 3] < 1 || d[i % 3] > 6) {
-                throw new IllegalArgumentException("Password never passed");
-            }
-            password[i] = d[i % 3];
-        }
-
-        if (password[indexFirstCube] != array[indexFirstCube] || password[indexSecondCube] != array[indexSecondCube]) {
-            return null;
-        } else {
-            return password;
-        }
     }
 }
